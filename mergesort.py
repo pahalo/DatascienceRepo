0f466ebcaa618994d3043ade366156b8abddf849
Paul Hankiewicz Lopez <paul@intranda.com>
2025-06-15 17:28:58 +0200

    Edit mergesort and add comments

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,56 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
+import matplotlib.pyplot as plt
 
+# Sortieren einer Liste durch Merchsort
+def mergeSort(arr):
+    # Wenn mehr als ein Element in der Liste ist
+    if len(arr) > 1:
+        # Mitte der Liste
+        mid = len(arr) // 2
+        
+        # Liste in zwei Hälften teilen
+        left = arr[:mid]
+        right = arr[mid:]
 
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
+        # Rekursives Sortieren der Hälften
         mergeSort(left)
         mergeSort(right)
 
-        l = 0
-        r = 0
-        i = 0
+        l = r = i = 0
 
+        # Elemente aus left und right sortiert zusammenführen
         while l < len(left) and r < len(right):
             if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+                arr[i] = left[l]
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                arr[i] = right[r]
                 r += 1
             i += 1
 
+        # Übrige Elemente aus left einfügen (falls vorhanden)
         while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
+            arr[i] = left[l]
             l += 1
             i += 1
 
+        # Übrige Elemente aus right einfügen (falls vorhanden)
         while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
+            arr[i] = right[r]
             r += 1
             i += 1
 
-
-import matplotlib.pyplot as plt
-
+# Liste zum durchlaufen
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+
+# Liste vor dem Sortieren visualisieren
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
+
+# mergeSort aufrufen
 mergeSort(my_list)
+
+# Liste nach dem Sortieren visualisieren
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
